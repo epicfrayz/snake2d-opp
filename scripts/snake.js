d98f062c35016f7bab401a6709b3c25db2da3f63
kaguya.ootsutsukii <61389918+Hepka@users.noreply.github.com>
2021-04-10 22:54:05 +0400

    Фикс маленькой недоработки
    Небольшой фикс маленькой недоработки:
    1. Если змейка идет вверх, то при нажатии на S она умирает и так в другие стороны (Если идет вправо, то при нажатии на A - умирает)
    2. Добавлено управление со стрелочек
    3. Изменена проверка нажатия на кнопки (С if...else на switch)

--- scripts/snake.js
+++ scripts/snake.js
@@ -1,8 +1,8 @@
 import Config from "./config.js";
 
 export default class Snake {
-	
-	constructor(){
+
+	constructor() {
 
 		this.config = new Config();
 		this.x = 160;
@@ -16,60 +16,60 @@ export default class Snake {
 
 	}
 
-	update( berry, score, canvas ) {
+	update(berry, score, canvas) {
 		this.x += this.dx;
 		this.y += this.dy;
-	
+
 		if (this.x < 0) {
 			this.x = canvas.element.width - this.config.sizeCell;
-		} else if ( this.x >= canvas.element.width ) {
+		} else if (this.x >= canvas.element.width) {
 			this.x = 0;
 		}
-	
+
 		if (this.y < 0) {
 			this.y = canvas.element.height - this.config.sizeCell;
-		} else if ( this.y >= canvas.element.height ) {
+		} else if (this.y >= canvas.element.height) {
 			this.y = 0;
 		}
-	
-		this.tails.unshift( { x: this.x, y: this.y } );
-	
-		if ( this.tails.length > this.maxTails ) {
+
+		this.tails.unshift({ x: this.x, y: this.y });
+
+		if (this.tails.length > this.maxTails) {
 			this.tails.pop();
 		}
-	
-		this.tails.forEach( (el, index) => {
-	
-			if ( el.x === berry.x && el.y === berry.y ) {
+
+		this.tails.forEach((el, index) => {
+
+			if (el.x === berry.x && el.y === berry.y) {
 				this.maxTails++;
 				score.incScore();
 				berry.randomPosition();
 			}
-	
-			for( let i = index + 1; i < this.tails.length; i++ ) {
-	
-				if ( el.x == this.tails[i].x && el.y == this.tails[i].y ) {
+
+			for (let i = index + 1; i < this.tails.length; i++) {
+
+				if (el.x == this.tails[i].x && el.y == this.tails[i].y) {
 					this.death();
 					score.setToZero();
 					berry.randomPosition();
 				}
-	
+
 			}
-	
-		} );
+
+		});
 
 	}
 
 	draw(context) {
 
-		this.tails.forEach( (el, index) => {
+		this.tails.forEach((el, index) => {
 			if (index == 0) {
 				context.fillStyle = "#FA0556";
 			} else {
 				context.fillStyle = "#A00034";
 			}
-			context.fillRect( el.x, el.y, this.config.sizeCell, this.config.sizeCell );
-		} );
+			context.fillRect(el.x, el.y, this.config.sizeCell, this.config.sizeCell);
+		});
 
 	}
 
@@ -85,20 +85,49 @@ export default class Snake {
 	}
 
 	control() {
-		
-		document.addEventListener("keydown",  (e) => {
-			if ( e.code == "KeyW" ) {
-				this.dy = -this.config.sizeCell;
-				this.dx = 0;
-			} else if ( e.code == "KeyA" ) {
-				this.dx = -this.config.sizeCell;
-				this.dy = 0;
-			} else if ( e.code == "KeyS" ) {
-				this.dy = this.config.sizeCell;
-				this.dx = 0;
-			} else if ( e.code == "KeyD" ) {
-				this.dx = this.config.sizeCell;
-				this.dy = 0;
+
+		document.addEventListener("keydown", (e) => {
+			switch (e.code) {
+				case "KeyW":
+					if (this.dy == this.config.sizeCell) return;
+					this.dy = -this.config.sizeCell;
+					this.dx = 0;
+					break;
+				case "KeyA":
+					if (this.dx == this.config.sizeCell) return;
+					this.dx = -this.config.sizeCell;
+					this.dy = 0;
+					break;
+				case "KeyS":
+					if (this.dy == -this.config.sizeCell) return;
+					this.dy = this.config.sizeCell;
+					this.dx = 0;
+					break;
+				case "KeyD":
+					if (this.dx == -this.config.sizeCell) return;
+					this.dx = this.config.sizeCell;
+					this.dy = 0;
+					break;
+				case "ArrowUp":
+					if (this.dy == this.config.sizeCell) return;
+					this.dy = -this.config.sizeCell;
+					this.dx = 0;
+					break;
+				case "ArrowLeft":
+					if (this.dx == this.config.sizeCell) return;
+					this.dx = -this.config.sizeCell;
+					this.dy = 0;
+					break;
+				case "ArrowDown":
+					if (this.dy == -this.config.sizeCell) return;
+					this.dy = this.config.sizeCell;
+					this.dx = 0;
+					break;
+				case "ArrowRight":
+					if (this.dx == -this.config.sizeCell) return;
+					this.dx = this.config.sizeCell;
+					this.dy = 0;
+					break;
 			}
 		});
 
