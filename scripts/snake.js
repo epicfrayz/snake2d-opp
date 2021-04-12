77d7cc56f7b6f6983a7079edd204a0aba9e2547f
kaguya.ootsutsukii <61389918+Hepka@users.noreply.github.com>
2021-04-12 15:56:46 +0400

    Update snake.js

--- scripts/snake.js
+++ scripts/snake.js
@@ -132,7 +132,7 @@ export default class Snake {
 					break;
 			}
 		});
-		var swiper = new Swipe("#switch-zone");
+		var swiper = new Swipe("#swipe-zone");
 		swiper.onUp(async() => {
 			console.log('[Control] Swiped UP!');
 			if (this.dy == this.config.sizeCell) return;
